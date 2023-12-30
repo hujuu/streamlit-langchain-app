2f287689d78cf036ea9b501a686b700b382330a7
hujuu <1961946+hujuu@users.noreply.github.com>
2023-12-30 18:46:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,13 @@ import streamlit as st
 st.title("langchain-streamlit-app")
 
 prompt = st.chat_input("What is up?")
-print(prompt)
+
+if prompt:  # 入力された文字列がある(Noneでも空文字列でもない)場合
+    # ユーザーのアイコンで
+    with st.chat_message("user"):
+        # promptをマークダウンとして整形して表示
+        st.markdown(prompt)
+    # AIのアイコンで
+    with st.chat_message("assistant"):
+        response = "こんにちは"  # 固定の応答を用意して
+        st.markdown(response) # 応答をマークダウンとして整形して表示
